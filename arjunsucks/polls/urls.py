22be6947992266a9eda85211a7bc7580e5c921a4
Arjun Bhalla <ab2383@cornell.edu>
2018-11-19 20:21:07 -0500

    added more detailed views and some templates

--- arjunsucks/polls/urls.py
+++ arjunsucks/polls/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 # Mapping to a URL to call the view function
-
-
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.results, name="results"),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
